61a40e489d45437140961d13953b33ba11ed8656
Pranav Darshan <65911046+PranavDarshan@users.noreply.github.com>
2026-03-12 16:19:18 +0530

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,6 +11,11 @@ sys.path.append(os.path.abspath(os.getcwd()))
 # access to the values within the .ini file in use.
 config = context.config
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
